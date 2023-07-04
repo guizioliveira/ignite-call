fb5c4ce7482f628f529280f5eb126ea71b0564b4
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-07-04 19:07:57 -0300

    feat: creating connection agenda page

---
+++ src/pages/register/connect-calendar/styles.ts
@@ -0,0 +1,18 @@
+import { Box, styled } from '@guizioliveira/ignite-ui-react'
+
+export const ConnectBox = styled(Box, {
+  marginTop: '$6',
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export const ConnectItem = styled('div', {
+  marginBottom: '$4',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  border: '1px solid $gray600',
+  padding: '$4 $6',
+  borderRadius: '$md',
+})
