63e827bb3e192872be343025272646a13c3bcafd
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-07-04 23:19:29 -0300

    feat: google authentication callbacks

--- src/pages/register/connect-calendar/styles.ts
+++ src/pages/register/connect-calendar/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@guizioliveira/ignite-ui-react'
+import { Box, Text, styled } from '@guizioliveira/ignite-ui-react'
 
 export const ConnectBox = styled(Box, {
   marginTop: '$6',
@@ -15,4 +15,9 @@ export const ConnectItem = styled('div', {
   border: '1px solid $gray600',
   padding: '$4 $6',
   borderRadius: '$md',
+})
+
+export const AuthError = styled(Text, {
+  color: '$danger-light',
+  marginBottom: '$4',
 })
